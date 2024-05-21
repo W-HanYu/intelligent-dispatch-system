d5a28b567bba3255d946dea4b41803ca8fc02760
wangtao <1453300745@qq.com>
2024-05-21 10:04:49 +0800

    feat: complete custom algorithm add

--- intelligent-dispatch-system/src/components/FileUploadModal.tsx
+++ intelligent-dispatch-system/src/components/FileUploadModal.tsx
@@ -1,7 +1,8 @@
 // src/FileUpload.tsx
 import React, { useState } from "react";
-import { Modal, Input, Button, Space, message } from "antd";
+import { Modal, Input, Button, Space, message, Tooltip } from "antd";
 import { uploadCustomAlgorithm } from "../interface";
+import { QuestionCircleOutlined } from "@ant-design/icons";
 
 interface UploadResponseData {
   messageData: string;
@@ -54,7 +55,22 @@ const FileUploadModal = ({ show, handleClose, handleFileUpload }: Props) => {
   return (
     <>
       <Modal
-        title="上传用户自定义算法文件"
+        title={
+          <>
+            上传用户自定义算法文件
+            <Tooltip
+              title="算法文件要求：
+              1. 目前仅支持c++(.cc)文件代码，如gwo.cc; 其他语言的上传无法识别；
+              2. 代码主函数命名必须是main(),因为后续调度会从这个入口开始；
+              3. 代码运行输出为output.txt文件，输出格式如下：
+              /1111145066001220777002005555599300AAA030006666/333333222222444444555555888888555555666666/AAAA6666111121000047777444447009999/0000000000000002222229991110AAAAAA888888999999/555001119999900AAA000007/988888888AAAAAA788888666666A3344333333555555/*46*328#30#20#20#50*
+              解释：1.两个//之间代表是某个机器上加工过的工件以及时间，总共用//隔开了6段数据，代表机器1-6；2.最后面的**之间代表的是最优解、计算时间以及后面#隔开的是输入参数，这里46是最优解、328是计算时间、#30#20#20#50输入参数；
+              "
+            >
+              <QuestionCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
+            </Tooltip>
+          </>
+        }
         open={show}
         onOk={onFileUpload}
         onCancel={handleClose}
